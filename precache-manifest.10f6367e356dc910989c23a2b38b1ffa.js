1cf1d739458aefac4d7c71189e6dd20343add195
Axel Rammon <axelrammon48@gmail.com>
2020-03-13 19:06:31 -0300

    Updates

---
+++ precache-manifest.10f6367e356dc910989c23a2b38b1ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29231e54838d2792fb59e9e6d402cf7b",
+    "url": "/get-grid/index.html"
+  },
+  {
+    "revision": "2871cb608ee8a0bcfaef",
+    "url": "/get-grid/static/css/main.36f1c7dc.chunk.css"
+  },
+  {
+    "revision": "509b5c5a5a7bc6365f3a",
+    "url": "/get-grid/static/js/2.c400ae1b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/get-grid/static/js/2.c400ae1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2871cb608ee8a0bcfaef",
+    "url": "/get-grid/static/js/main.da318565.chunk.js"
+  },
+  {
+    "revision": "55a6caf0f7d91892bf3e",
+    "url": "/get-grid/static/js/runtime-main.7689290c.js"
+  }
+]);
